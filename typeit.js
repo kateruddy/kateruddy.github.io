b4253d554d5eaa1dd92f284fd346ae97de8e8244
kateruddy <119622152+kateruddy@users.noreply.github.com>
2023-04-26 16:25:14 -0400

    formatting fixes and new ordinary content

--- typeit.js
+++
@@ -1,6 +0,0 @@
-new TypeIt("#cta", {
-  strings: ["Soon-to-be Boston University alumna.", "Ex-actor.", "Juvenile advertiser.", "Click to learn more."],
-  speed: 50,
-  waitUntilVisible: true,
-  
-}).go();
